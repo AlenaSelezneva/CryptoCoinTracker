6c8bd700cdf599035aeb703b2dcaf50c6f7aa4a1
AlenaSelezneva <alena.selezneva.678@gmail.com>
2021-04-11 11:40:03 -0300

    using repository

--- CoinTrackerStarter/app/src/main/java/ca/burchill/cointracker/viewModels/CoinListViewModel.kt
+++ CoinTrackerStarter/app/src/main/java/ca/burchill/cointracker/viewModels/CoinListViewModel.kt
@@ -5,11 +5,13 @@ import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import ca.burchill.cointracker.database.getDatabase
 import ca.burchill.cointracker.domain.Coin
 import ca.burchill.cointracker.network.CoinApi
 import ca.burchill.cointracker.network.CoinApiResponse
 import ca.burchill.cointracker.network.NetworkCoin
 import ca.burchill.cointracker.network.asDomainModel
+import ca.burchill.cointracker.repository.CoinsRepository
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
@@ -23,7 +25,17 @@ import retrofit2.await
 enum class CoinApiStatus { LOADING, ERROR, DONE }
 
 
-class CoinListViewModel() : ViewModel() {
+class CoinListViewModel(application: Application) : AndroidViewModel(application) {
+
+//    private var _eventNetworkError = MutableLiveData<Boolean>(false)
+//    val eventNetworkError: LiveData<Boolean>
+//        get() = _eventNetworkError
+//
+//
+//    private var _isNetworkErrorShown = MutableLiveData<Boolean>(false)
+//    val isNetworkErrorShown: LiveData<Boolean>
+//        get() = _isNetworkErrorShown
+
 
     // The internal MutableLiveData that stores the status of the most recent request
     private val _status = MutableLiveData<CoinApiStatus>()
@@ -31,11 +43,13 @@ class CoinListViewModel() : ViewModel() {
         get() = _status
 
 
-    private val _coins = MutableLiveData<List<Coin>>()
-    val coins: LiveData<List<Coin>>
-        get() = _coins
+//    private val _coins = MutableLiveData<List<Coin>>()
+//    val coins: LiveData<List<Coin>>
+//        get() = _coins
 
+    private val coinsRepository = CoinsRepository(getDatabase(application ))
 
+    val coins = coinsRepository.coins
 
     // or use viewModelScope
     private var viewModelJob = Job()
@@ -50,10 +64,13 @@ class CoinListViewModel() : ViewModel() {
 
        coroutineScope.launch {
             try {
-                var coinResult = CoinApi.retrofitService.getCoins()
-                if (coinResult.coins.size > 0) {
-                    _coins.value = asDomainModel( coinResult.coins)
-                }
+//                var coinResult = CoinApi.retrofitService.getCoins()
+//                if (coinResult.coins.size > 0) {
+//                    _coins.value = asDomainModel( coinResult.coins)
+//                }
+                coinsRepository.refreshCoins()
+//                _eventNetworkError.value = false
+//                _isNetworkErrorShown.value = false
             } catch (t: Throwable) {
                _status.value = CoinApiStatus.ERROR
             }
